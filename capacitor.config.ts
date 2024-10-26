d431d4614f322d207f0c1bceb09b60e25a27fd10
Pablo Gutiérrez <pa.gutierrezu@duocuc.cl>
2024-10-26 11:46:58 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PabloGutierrez154670173',
+  webDir: 'www'
+};
+
+export default config;
