98b383fefc3cc497f68956ea266e63192cc99a48
Pablo Gutiérrez <pa.gutierrezu@duocuc.cl>
2024-10-26 11:58:04 -0300

    Se completa codigo de servicio, se crea interface

--- src/app/services/peliculas-api.service.ts
+++ src/app/services/peliculas-api.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Pelicula } from '../interfaces/pelicula';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PeliculasApiService {
 
-  constructor() { }
+  private readonly apiUrl: string = 'https://lightgrey-owl-901213.hostingersite.com/api/get_peliculas.php?limit=100'
+  constructor(private readonly http: HttpClient) {}
+
+  obtenerPeliculas(): Observable<Pelicula[]>{
+    return this.http.get<Pelicula[]>(this.apiUrl);
+  } //Agregar Observable
 }
